1893402f4bcb22eb6e4607c67c48c75bcdd04980
David Pham <davidpham@Davids-Mac-mini.local>
2021-03-19 13:17:24 +0100

    feat: tab's not working on macosx

--- personal/swiss-keybindings.el
+++ personal/swiss-keybindings.el
@@ -75,12 +75,7 @@
 (define-key key-translation-map (kbd "Ŧ") (kbd "M-T"))
 (global-set-key (kbd "M-T")  'sp-transpose-sexp)
 
-(global-set-key (kbd "<f9>") 'tab-bar-mode)
-(global-set-key (kbd "C-T") 'tab-bar-mode)
-
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
-
 
 ;; ;; Rebind altgr + key to the correct default behavior
 ;; ;; To get the same alt as the american keyboard
