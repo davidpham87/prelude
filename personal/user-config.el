737d49f4b275f0c01cfd30acf3cd83cfeb023070
David Pham <davidpham87@gmail.com>
2021-03-28 14:21:34 +0200

    feat: update

--- personal/user-config.el
+++ personal/user-config.el
@@ -15,15 +15,71 @@
   (eshell nil)
   (rename-buffer "*eshell*<shadow-cljs>"))
 
-;; (require 'exwm)
-;; (require 'exwm-config)
-;; (exwm-config-default)
-
 (prelude-require-package 'pandoc-mode)
 
 (add-hook 'markdown-mode-hook 'pandoc-mode)
 (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
 
-(setenv "PATH" (concat "/Users/davidpham/bin:" (getenv "PATH")))
+(use-package pdf-tools
+  :ensure t
+  :config
+  (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer))
 
-(setq default-directory "/Users/davidpham")
+(use-package exwm
+  :if (string= (getenv "exwm_x_enable") "yes")
+  :ensure t
+  :config
+
+  (setq exwm-workspace-number 5)
+
+  (setq exwm-input-prefix-keys
+        '(?\C-x
+          ?\C-u
+          ?\C-h
+          ?\M-x
+          ?\M-`
+          ?\M-&
+          ?\M-:
+          ?\C-\M-j  ;; Buffer list
+          ?\C-\ ))
+
+  ;; Set up global key bindings.  These always work, no matter the input state!
+  ;; Keep in mind that changing this list after EXWM initializes has no effect.
+  (setq exwm-input-global-keys
+        `(
+          ;; Reset to line-mode (C-c C-k switches to char-mode via exwm-input-release-keyboard)
+          ([?\s-r] . exwm-reset)
+
+          ;; Move between windows
+          ([s-left] . windmove-left)
+          ([s-right] . windmove-right)
+          ([s-up] . windmove-up)
+          ([s-down] . windmove-down)
+
+          ;; Launch applications via shell command
+          ([?\s-&] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))
+
+          ;; Switch workspace
+          ([?\s-w] . exwm-workspace-switch)
+          ([?\s-`] . (lambda () (interactive) (exwm-workspace-switch-create 0)))
+
+          ;; 's-N': Switch to certain workspace with Super (Win) plus a number key (0 - 9)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
+
+  (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app)
+
+  (exwm-enable))
+
+(defun dph.windows/make (n)
+  (interactive "nNumber of split: ")
+  (delete-other-windows)
+  (dotimes (i (- n 1))
+    (split-window-right))
+  (balance-windows))
