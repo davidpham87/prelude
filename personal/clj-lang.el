016dd7babdc67cd424d38b695f77c0910cf0c912
David Pham <davidpham87@gmail.com>
2021-10-06 11:10:59 +0200

    feat: add of clojure refactor

--- personal/clj-lang.el
+++ personal/clj-lang.el
@@ -17,6 +17,17 @@
   :config
   (global-set-key (kbd "C-è") 'cider-find-dwim))
 
+(use-package clj-refactor
+  :after (cider)
+  :ensure t
+  :config
+  (defun clj-refactor-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c m"))
+  (add-hook 'clojure-mode-hook #'clj-refactor-mode-hook))
+
 (defun bb-nrepl (port)
   (interactive "sPort: ")
   (make-process
