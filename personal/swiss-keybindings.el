6265f3a847cb71d689d66ffe635f0d3018fffcd6
David Pham <david@mac.lan>
2025-11-29 17:00:14 +0100

    Update

--- personal/swiss-keybindings.el
+++ personal/swiss-keybindings.el
@@ -1,5 +1,4 @@
 (require 'iso-transl)
-(use-package bufler)
 ;; (use-package paredit-mode)
 
 (global-set-key (kbd "C-§") 'other-frame)
@@ -21,14 +20,15 @@
 
 ;; Rebind altgr + key to the correct default behavior
 ;; To get the same alt as the american keyboard
-(let* ((km '(("«" "M-y") ("»" "M-x") ("¢" "M-c") ("“" "M-v") ("”" "M-b")
-             ("æ" "M-a") ("ß" "M-s") ("ð" "M-d") ("đ" "M-f") ("ŋ" "M-g")
-             ("ł" "M-w") ("€" "M-e") ("¶" "M-r") ("ŧ" "M-t")))
-       (kmc (mapcar (lambda(y) (--map (concat "C-" it) y)) km)) ; C- variant
-       (key-mapper (lambda (it) (define-key key-translation-map
-                                  (kbd (car it)) (kbd (car (cdr it)))))))
-  (mapc key-mapper km)
-  (mapc key-mapper kmc))
+;; (let* ((km '(("«" "M-y") ("»" "M-x") ("¢" "M-c") ("“" "M-v") ("”" "M-b")
+;;              ("æ" "M-a") ("ß" "M-s") ("ð" "M-d") ("đ" "M-f") ("ŋ" "M-g")
+;;              ("ł" "M-w") ("€" "M-e") ("¶" "M-r") ("ŧ" "M-t")))
+;;        (kmc (mapcar (lambda(y) (--map (concat "C-" it) y)) km)) ; C- variant
+;;        (key-mapper (lambda (it) (define-key key-translation-map
+;;                                   (kbd (car it)) (kbd (car (cdr it)))))))
+;;   (mapc key-mapper km)
+;;   (mapc key-mapper kmc))
+
 
 ;; (let* ((km '(("¥" "M-y") ("≈" "M-x") ("©" "M-c") ("√" "M-v") ("∫" "M-b")
 ;;              ("å" "M-a") ("ß" "M-s") ("∂" "M-d") ("ƒ" "M-f")
@@ -39,13 +39,13 @@
 ;;   (mapc key-mapper km)
 ;;   (mapc key-mapper kmc))
 
-;; ;; This is to make mac friendly
-
 ;; Mac settings to have the same feeling as pc keyboard
 (global-set-key (kbd "M-à") '"{")
-(global-set-key (kbd "M-è") '"[")
 (global-set-key (kbd "M-$") '"}")
+(global-set-key (kbd "M-è") '"[")
+(global-set-key (kbd "‘") '"]")
 (global-set-key (kbd "C-<") '"\\")
+
 
 ;; Emacs-live rewrite
 (global-set-key (kbd "C-h") 'help-command)
@@ -54,9 +54,9 @@
 ;; Make C-é delete backward
 (global-set-key (kbd "C-é") 'delete-backward-char)
 (global-set-key (kbd "<C-backspace>") 'delete-backward-char)
-;; (global-set-key [(control ?h)] 'delete-backward-char) ;; in terminals
+
 (global-set-key (kbd "M-n")  'back-to-indentation)
-;; (define-key paredit-mode-map (kbd "C-é") 'paredit-backward-delete)
+
 
 ;;allow the deletion of words:
 ;;backward kill word (forward kill word is M-d)
@@ -92,15 +92,7 @@
 ;;   (mapc key-mapper km)
 ;;   (mapc key-mapper kmc))
 
-;; ;; This is to make mac friendly
-;; (setq mac-function-modifier 'control
-;;       mac-command-modifier 'control
-;;       mac-right-command-modifier 'control
-;;       mac-control-modifier 'meta
-;;       mac-right-option-modifier 'option)
 
-;; ;; Mac settings to have the same feeling as pc keyboard
-;; (global-set-key (kbd "M-à") '"{")
-;; (global-set-key (kbd "M-è") '"[")
-;; (global-set-key (kbd "M-$") '"}")
-;; (global-set-key (kbd "C-<") '"\\")
+;; ;; This is to make mac friendly
+(setq mac-command-modifier 'meta)
+
